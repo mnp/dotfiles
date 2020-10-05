e68905a2ee8ed8b8415858890124bdc671427411
Mitchell Perilstein <mitchell@wallaroolabs.com>
2020-10-05 11:02:38 -0400

    Update to align with Wiki around standards

--- shared-elisp/my-calc-extras.el
+++ shared-elisp/my-calc-extras.el
@@ -1,18 +1,32 @@
+;;
+;; I'm not sure why these aren't part of `math-standard-units`.
+;; Source: https://en.wikipedia.org/wiki/Mebibyte
+;;
 
-;;; Custom units stored by Calc on Fri Mar  6 14:10:18 2015
 (setq math-additional-units '(
+  ;; Base units. Note "b" is Barns in math-standard-units, while convention "B" is Bytes, so
+  ;; there's no conflict.
   (bits nil "bits")
-  (bytes "8 * bits" "bytes")
+  (B "8 * bits" "Bytes")
 
-  (tib "1000 * gib"    "TiB")
-  (gib "1000 * mib"    "GiB")
-  (mib "1000 * kib"    "MiB")
-  (kib "1000 * bytes"  "KiB")
+  ;; Decimal units
+  (kB  "1000   B"  "kilobyte")
+  (MB  "1000^2 B"  "megabyte")
+  (GB  "1000^3 B"  "gigabyte")
+  (TB  "1000^4 B"  "terabyte")
+  (PB  "1000^5 B"  "petabyte")
+  (EB  "1000^6 B"  "exabyte")
+  (ZB  "1000^7 B"  "zettabyte")
+  (YB  "1000^8 B"  "yottabyte")
 
-  (tb  "1024 * gb"      "TB")
-  (gb  "1024 * mb"      "GB")
-  (mb  "1024 * kb"      "MB")
-  (kb  "1024 * bytes"   "KB")
-))
-;;; End of custom units
+  ;; Binary units
+  (KiB  "1024   B"  "kibibyte")
+  (MiB  "1024^2 B"  "mebibyte")
+  (GiB  "1024^3 B"  "gibibyte")
+  (TiB  "1024^4 B"  "tebibyte")
+  (PiB  "1024^5 B"  "pebibyte")
+  (EiB  "1024^6 B"  "exbibyte")
+  (ZiB  "1024^7 B"  "zebibyte")
+  (YiB  "1024^8 B"  "yobibyte")))
 
+(setq math-units-table nil)
