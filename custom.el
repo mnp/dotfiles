f5728c5161abe7e28b354aa3b2e850d9f360668c
Mitchell Perilstein <mitchell@wallaroolabs.com>
2020-11-25 19:34:46 -0500

    Incremental, still need to port more from old

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(fancy-narrow diminish use-package)))
+ '(package-selected-packages
+   '(helm-grepint lsp-mode cargo kotlin-mode default-text-scale rustic ob-rust toml-mode ponylang-mode restclient search-web smart-backspace yaml-mode dockerfile-mode zetteldeft deft fancy-narrow diminish use-package))
+ '(safe-local-variable-values '((my-grep-root . "~/prj/wallaroo-plus/fitzroy"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
