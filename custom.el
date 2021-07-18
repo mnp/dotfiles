f92674b3d3728ac9487ff29572c77972923697f9
Mitchell Perilstein <mitchell.perilstein@gmail.com>
2021-07-18 10:00:54 -0400

    No beep. Highlight mode for yaml and python.

--- custom.el
+++ custom.el
@@ -3,12 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(gnutls-algorithm-priority "normal:-vers-tls1.3")
+ '(lsp-kotlin-language-server-path "~/prj/kotlin-language-server/00----runme")
  '(package-selected-packages
-   '(helm-grepint lsp-mode cargo kotlin-mode default-text-scale rustic ob-rust toml-mode ponylang-mode restclient search-web smart-backspace yaml-mode dockerfile-mode zetteldeft deft fancy-narrow diminish use-package))
- '(safe-local-variable-values '((my-grep-root . "~/prj/wallaroo-plus/fitzroy"))))
+   (quote
+    (highlight-indentation org-tempo persistent-scratch helm-grepint lsp-mode cargo kotlin-mode default-text-scale rustic ob-rust toml-mode ponylang-mode restclient search-web smart-backspace yaml-mode dockerfile-mode zetteldeft deft fancy-narrow diminish use-package)))
+ '(safe-local-variable-values (quote ((my-grep-root . "~/prj/wallaroo-plus/fitzroy")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(helm-selection ((t :background "LightYellow" :foreground "black"))))
